9a29617673d15d47c035ac7b9fde982fde793ae2
Hafiz Shehbaz Ali <shehbaz.bsee1395@gmail.com>
2021-05-01 23:20:25 +0400

    code added

--- app/src/main/java/com/aalianfamily/problem11/MainActivity.java
+++ app/src/main/java/com/aalianfamily/problem11/MainActivity.java
@@ -3,12 +3,48 @@ package com.aalianfamily.problem11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "MainActivity44";
+    private MessageListener mMessageListener;
+    private Message mMessage;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMessageListener = new MessageListener() {
+            @Override
+            public void onFound(Message message) {
+                Log.d(TAG, "Found message: " + new String(message.getContent()));
+            }
+
+            @Override
+            public void onLost(Message message) {
+                Log.d(TAG, "Lost sight of message: " + new String(message.getContent()));
+            }
+        };
+
+        mMessage = new Message("Hello World".getBytes());
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Nearby.getMessagesClient(this).publish(mMessage);
+        Nearby.getMessagesClient(this).subscribe(mMessageListener);
+    }
+
+    @Override
+    public void onStop() {
+        Nearby.getMessagesClient(this).unpublish(mMessage);
+        Nearby.getMessagesClient(this).unsubscribe(mMessageListener);
+        super.onStop();
+    }
+
+
 }
